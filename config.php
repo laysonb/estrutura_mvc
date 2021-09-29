3ce5d9ca13a10ed52472503c9811bf66385456f2
Ilandio Joaquim <laysonbelinho@gmail.com>
2021-09-29 05:40:30 +0100

    projecto mvc

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php
+require "einvironment.php";
+$config = array();
+if(ENVIRONMENT =="development"){
+    define("BASE_URL","http://localhost/estruturamvc/");
+    $config['dbname'] ='estruturamvc';
+    $config['host']   ="localhost";
+    $config['dbuser'] ="root";
+    $config['dbpass'] ="";
+}else{
+    define("BASE_URL","http://milant.com/");
+    $config['dbname'] ='estruturamvc';
+    $config['host']   ="localhost";
+    $config['dbuser'] ="root";
+    $config['dbpass'] ="";
+}
+global $db;
+try {
+    $db = new PDO("myslql:dbname=".$config['dbname'].";host".$config['host'],$config['dbuser'],$config['dbpass']);
+} catch (PDOException $e) {
+    echo "Error".$e->getMessage();
+}
